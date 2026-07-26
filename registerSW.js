58cc0a54241659aa99139cb8188eb1a9dbc18c76
riodwierland <riodwierland@users.noreply.github.com>
2026-07-26 17:54:53 +0000

    deploy: fdd925feb9bc92e281ef6ea8b2ceda326e508f76

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Web-App/couple-tracker/sw.js', { scope: '/Web-App/couple-tracker/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Web-App/sw.js', { scope: '/Web-App/' })})}
